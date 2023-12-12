7dd9eafe7686a71a342ab6be94db6fcfd4ed53d4
AmrAbdulSalam <amrsalam@hotmail.com>
2023-12-12 22:44:34 +0200

    final task

--- Program.cs
+++ Program.cs
@@ -5,7 +5,28 @@ namespace Task
     {
         public static void Main()
         {
-            
+            var arr = new[] { 1, 2, 3, 4 };
+
+            var obj = new NumbProc(arr);
+
+            obj
+                .Add(5)
+                .Sub(1)
+                .Div(1);
+
+            obj.DisplayArray();
+
+
+            var arr2 = new[] { 1, 2, 3, 4 };
+
+            var obj2 = new NumbProc2(arr2);
+
+            obj2
+                .Add(5)
+                .Sub(1)
+                .Div(1);
+
+            obj2.DisplayArray();
         }
     }
 }
